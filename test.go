a171ebae997f2c735796e423a5c8593b1c75ea83
atabekdemurtaza <mychannel21war@gmail.com>
2024-12-07 09:09:20 +0000

    Let's get practice with golang

---
+++ test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//	func add(x float64, y float64) float64 {
+//		return math.Sqrt(float64(x*x + y*y))
+//	}
+//
+//	func main() {
+//		fmt.Println(add(1, 2))
+//	}
+
+//func swap(x, y string) (string, string) {
+//	return y, x
+//}
+//
+//func main() {
+//	a, b := swap("hello", "world")
+//	fmt.Println(a, b)
+//}
+
+//import "fmt"
+//
+//func split(sum int) (x, y int) {
+//	x = sum * 4 / 9
+//	y = sum - x
+//	return
+//}
+//
+//func main() {
+//	fmt.Println(split(17))
+//}
+
+//import "fmt"
+//
+//var c, python, java bool
+//
+//func main() {
+//	var i int
+//	fmt.Println(i, c, python, java)
+//}
+
+//import "fmt"
+//
+//var i, j int = 1, 2
+//
+//func main() {
+//	var c, python, java = true, false, "no!"
+//	k := 3
+//	fmt.Println(i, j, c, python, java, k)
+//}
+
+//import "fmt"
+//
+//func main() {
+//	var i int
+//	var j float32
+//	var b bool
+//	var s string
+//	fmt.Printf("%v %v %v %q\n", i, j, b, s)
+//}
+
+//import (
+//	"fmt"
+//	"math"
+//)
+//
+//func main() {
+//	var x, y int = 3, 4
+//	var f float64 = math.Sqrt(float64(x*x + y*y))
+//	var z uint = uint(f)
+//	fmt.Println(x, y, z, f)
+//
+//	v := 42 // change me!
+//	fmt.Printf("v is of type %T\n", v)
+//}
+
+//func main() {
+//	sum := 1
+//	for sum < 1000 {
+//		sum += sum
+//	}
+//	fmt.Println(sum)
+//}
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	fmt.Println(sqrt(2))
+}
